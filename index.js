16ba8cd645e463f483adff6b56edb27a7af47692
gabrielmororo <gabrielmororo@alunos.utfpr.edu.br>
2025-11-22 13:46:12 -0300

    chore: add pg setup dotenv setup

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const name = 'finance-app-api'
+import 'dotenv/config.js'
+import express from 'express'
+import { pool } from './src/db/postgres/client.js'
 
-console.log(`Project Name: ${name}`)
+const app = express()
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('select * from users')
+        res.send(JSON.stringify(result.rows))
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
